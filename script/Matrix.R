991afe4efad0073ff3b2e6c82cc6dabe096dfba0
rmateosr <141889080+rmateosr@users.noreply.github.com>
2024-11-16 16:19:19 +0900

    Update Matrix.R

--- script/Matrix.R
+++ script/Matrix.R
@@ -1,8 +1,7 @@
 #!/usr/bin/Rscript
 args = commandArgs(trailingOnly=TRUE)
 # List of required packages
-required_packages <- c("stringr", "data.table", "dplyr",
-                       "parallel", "Matrix", "foreach", "ggsci")
+required_packages <- c("stringr", "tools", "data.table")
 
 # Function to install missing packages
 install_if_missing <- function(packages) {
@@ -17,27 +16,28 @@ install_if_missing <- function(packages) {
 # Install and load required packages
 install_if_missing(required_packages)
 library(stringr)
-library(data.table)
-library(dplyr)
-library(parallel)
+library(tools)
 library(Matrix)
-
+library(data.table)
 
 alteredSJnames <- readRDS("resources/SJnames.RDS")
-sample <- readRDS(args[1])
+sample <- as.data.frame(fread(args[1]))
 # Extract the file name without extension
-file_name <- tools::file_path_sans_ext(basename(args[1]))
+file_name <- file_path_sans_ext(basename(args[1]))
 
 
-SJinfo = data.frame(sample[[1]])
-sampleinfo = data.frame(sample[[2]])
+rownames(sample) = sample[,1]
+SJinfo = sample[,2]
+sample = sample[,-(1:2)]
+sampleinfo = colnames(sample)
+
 
 alteredSJnames = alteredSJnames[alteredSJnames!= "Sample_name"]
 alteredSJcoords =  str_split_fixed(alteredSJnames, "[:-]", 4)
 alteredstarts = apply(cbind(alteredSJcoords[,1], ":", alteredSJcoords[,2]),1,paste0, collapse = "")
 alteredends = apply(cbind(alteredSJcoords[,1], ":", alteredSJcoords[,3]),1,paste0, collapse = "")
-notannotatedarrayfromjxn =  sample[[3]][SJinfo$annotated==0, ,drop = FALSE]
-alteredSJappearing = as.matrix(sample[[3]][rownames(sample[[3]]) %in% alteredSJnames & SJinfo$annotated==0,] )
+notannotatedarrayfromjxn =  sample[SJinfo==0, ,drop = FALSE]
+alteredSJappearing = as.matrix(sample[rownames(sample) %in% alteredSJnames & SJinfo==0,] )
 
 
 AlteredthisProject = matrix(0, ncol = length(alteredSJnames), nrow = dim(notannotatedarrayfromjxn)[2])
@@ -49,7 +49,7 @@ for(cont in 1:dim(AlteredthisProject)[2]){
     AlteredthisProject[,cont] =  t(alteredSJcountsinthisproject)
   }
 }
-annotatedarrayfromjxn =  sample[[3]][SJinfo$annotated==1, ,drop = FALSE]
+annotatedarrayfromjxn =  sample[SJinfo==1, ,drop = FALSE]
 if(dim(annotatedarrayfromjxn)[1] > 0){
   SJcoords =  str_split_fixed(rownames(annotatedarrayfromjxn), "[:-]", 4)
   SJstarts = apply(cbind(SJcoords[,1], ":", SJcoords[,2]),1,paste0, collapse = "")
@@ -95,4 +95,4 @@ Normal = startsthisProject + endsthisProject
 Altered = AlteredthisProject
 Total = Normal + Altered
 dataset = list(Altered, Total,sampleinfo)
-saveRDS(dataset, paste0("input/", file_name,  ".RDS"))
+saveRDS(dataset, paste0("tmp/", file_name,  ".RDS"))
